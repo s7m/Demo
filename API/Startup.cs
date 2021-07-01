be1d00948ddffc1f13a34255b384e2c9173d7090
Shaheen7m <shaheen86@gmail.com>
2021-07-01 16:51:54 +0100

    Added angular client, bootstrap and font awesome

--- API/Startup.cs
+++ API/Startup.cs
@@ -65,7 +65,7 @@ namespace API
             {
                 opt.AddPolicy("CorsPolicy", policy =>
                  {
-                     policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
+                     policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200");
                  });
             });
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
