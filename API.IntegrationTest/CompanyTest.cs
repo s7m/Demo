2aa16e45ecc9008e6df8706d08d5a9d4e0642922
Shaheen7m <shaheen86@gmail.com>
2021-07-05 00:20:57 +0100

    Added integration test

---
+++ API.IntegrationTest/CompanyTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace API.IntegrationTest
+{
+    public class CompanyTest : IClassFixture<TestFixture<Startup>>
+    {
+        private HttpClient Client;
+
+        public CompanyTest(TestFixture<Startup> fixture)
+        {
+            Client = fixture.Client;
+        }
+
+        [Fact]
+        public async Task TestGetCompanyAsync()
+        {
+            // Arrange
+            var request = "/api/Company";
+
+            // Act
+            var response = await Client.GetAsync(request);
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+        }
+    }
+}
